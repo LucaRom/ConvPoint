a30faa912c31d9d15bd0c583d2c86e4b2a9c159a
Mathieu <mathieu.turgeon-pelchat@canada.ca>
2020-02-26 10:29:00 -0500

    file formatting

--- examples/s3dis/prepare_s3dis_label.py
+++ examples/s3dis/prepare_s3dis_label.py
@@ -10,8 +10,8 @@ import argparse
 # BASE_DIR = os.path.join(os.path.dirname(__file__),'../../../data/Stanford3dDataset_v1.2_Aligned_Version')
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--folder', '-f', help='Path to data folder')
-parser.add_argument("--dest")
+parser.add_argument('--folder', '-f', default='', help='Path to data folder')
+parser.add_argument("--dest", '-d', default='', help='Path to destination folder')
 args = parser.parse_args()
 BASE_DIR = args.folder
 
@@ -30,18 +30,17 @@ object_dict = {
             'bookcase': 11,
             'board':    12}
 
-path_Dir_Areas =  os.listdir(BASE_DIR)
+path_Dir_Areas = os.listdir(BASE_DIR)
 
 for Area in path_Dir_Areas:
-    if not os.path.isdir(os.path.join(BASE_DIR,Area)):
+    if not os.path.isdir(os.path.join(BASE_DIR, Area)):
         continue
-    path_Dir_Rooms = os.listdir(os.path.join(BASE_DIR,Area))
+    path_Dir_Rooms = os.listdir(os.path.join(BASE_DIR, Area))
     for Room in path_Dir_Rooms:
 
-
-        xyz_Room = np.zeros((1,6))
-        label_Room = np.zeros((1,1))
-        path_Annotations = os.path.join(BASE_DIR,Area,Room,"Annotations")
+        xyz_Room = np.zeros((1, 6))
+        label_Room = np.zeros((1, 1))
+        path_Annotations = os.path.join(BASE_DIR, Area, Room, "Annotations")
         if not os.path.exists(path_Annotations):
             print("Error {} does not exists".format(path_Annotations))
             continue
@@ -49,26 +48,26 @@ for Area in path_Dir_Areas:
         print(path_Annotations)
         # make store directories
         # path_prepare_label = os.path.join("../../../data/S3DIS/prepare_label_rgb",Area,Room)
-        path_prepare_label = os.path.join(args.dest,Area,Room)
+        path_prepare_label = os.path.join(args.dest, Area, Room)
         if not os.path.exists(path_prepare_label):
             os.makedirs(path_prepare_label)
         #############################
         path_objects = os.listdir(path_Annotations)
         for Object in path_objects:
-            if Object.split("_",1)[0] in object_dict:
-                print(Object.split("_",1)[0] + " value:" ,object_dict[Object.split("_",1)[0]])
-                xyz_object = np.loadtxt(os.path.join(path_Annotations,Object))[:,:]#(N,6)
-                label_object = np.tile([object_dict[Object.split("_",1)[0]]],(xyz_object.shape[0],1))#(N,1)
+            if Object.split("_", 1)[0] in object_dict:
+                print(f"{Object.split('_', 1)[0]} value: {object_dict[Object.split('_',1)[0]]}")
+                xyz_object = np.loadtxt(os.path.join(path_Annotations, Object))[:, :]  # (N,6)
+                label_object = np.tile([object_dict[Object.split("_", 1)[0]]], (xyz_object.shape[0], 1))  # (N,1)
             else:
                 continue
 
-            xyz_Room = np.vstack((xyz_Room,xyz_object))
-            label_Room = np.vstack((label_Room,label_object))
+            xyz_Room = np.vstack((xyz_Room, xyz_object))
+            label_Room = np.vstack((label_Room, label_object))
 
-        xyz_Room = np.delete(xyz_Room,[0],0)
-        label_Room = np.delete(label_Room,[0],0)
+        xyz_Room = np.delete(xyz_Room, [0], 0)
+        label_Room = np.delete(label_Room, [0], 0)
 
-        np.save(path_prepare_label+"/xyzrgb.npy",xyz_Room.astype(np.float16))
-        np.save(path_prepare_label+"/label.npy",label_Room)
+        np.save(path_prepare_label+"/xyzrgb.npy", xyz_Room.astype(np.float16))
+        np.save(path_prepare_label+"/label.npy", label_Room)
 
 
