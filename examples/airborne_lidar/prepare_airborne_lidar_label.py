6b5a47b4ba45452c02338a3ca185e61d374bad26
mathieu.tp <mathieu.turgeon-pelchat@canada.ca>
2020-08-19 10:36:02 -0400

    Update preparation step

--- examples/airborne_lidar/prepare_airborne_lidar_label.py
+++ examples/airborne_lidar/prepare_airborne_lidar_label.py
@@ -92,7 +92,7 @@ def main():
 
             xyzni, label, nb_pts = read_las_format(base_dir / f"{elem}.las")
 
-            write_features(f"{path_prepare_label / elem.split('.')[0]}_prepared.hdfs", xyzni=xyzni, labels=label)
+            write_features(Path(f"{path_prepare_label / elem.split('.')[0]}_prepared.hdfs"), xyzni=xyzni, labels=label)
             print(f"File {dst}/{elem} prepared. {nb_pts:,} points written.")
 
 
